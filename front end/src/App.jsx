e4f39a9259d93f44255abd71e6b178a862128da0
romain vanacker <romain.vanacker@gmail.com>
2025-07-17 12:25:35 +0200

    backend1

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>J'adore les cours d'anglais</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
